ddb78ddcbf08942bdba253598f9ff243e6d1625c
Rewel Garcia <rewelgarcia@Rewels-MacBook-Air.local>
2018-01-05 08:24:22 -0800

    w4d5

--- w4d4/rails-auth/rails-auth/app/models/user.rb
+++ w4d4/rails-auth/rails-auth/app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
-  validates :session_token, presence: true
   validates :password_digest, presence: { message: 'Password can\'t be blank'}
+  validates :session_token, presence: true, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
   before_validation :ensure_session_token
+
+  attr_reader :password
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
